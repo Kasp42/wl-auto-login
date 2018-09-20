f49ccb60d789b374a96597063f4b96d8721a4c3d
Vladislav <Kasp42@users.noreply.github.com>
2018-09-20 15:49:15 +0300

    Update wl-auto-login.user.js

--- wl-auto-login.user.js
+++ wl-auto-login.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wellnessliving AutoLogin
 // @namespace    https://dev.1024.info/
-// @version      0.7
+// @version      0.8
 // @description  Log in WL/prg with password from studio.
 // @author       Vladislav Kobzev
 // @match        *://*.wellnessliving.com/*
